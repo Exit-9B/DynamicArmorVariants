721a798f97252bf43bc409a715e846ba08476127
Parapets <southwest85b@gmail.com>
2022-04-03 11:13:50 +0100

    Create FOMOD installer

---
+++ Build.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+md "package\SkyrimSE"
+md "package\SkyrimVR"
+md "package\Data"
+md "package\UIExtensions_Menu"
+
+cmake --preset vs2019-windows --check-stamp-file "build/CMakeFiles/generate.stamp" || goto :error
+cmake --build build --config Release || goto :error
+cmake --install build --component "Fomod" --prefix "package" || goto :error
+cmake --install build --component "Data" --prefix "package/Data" || goto :error
+cmake --install build --component "UIExtensions_Menu" --prefix "package/UIExtensions_Menu" || goto :error
+cmake --install build --component "SKSEPlugin" --prefix "package/SkyrimSE" || goto :error
+
+cmake --preset vs2019-windows-vr --check-stamp-file "buildVR/CMakeFiles/generate.stamp" || goto :error
+cmake --build buildVR --target "DynamicArmorVariants" --config Release || goto :error
+cmake --install buildVR --component "SKSEPlugin" --prefix "package/SkyrimVR" || goto :error
+
+pushd package
+7z a -r -t7Z "..\DynamicArmorVariants.7z" *
+popd
+
+goto :EOF
+
+:error
+exit /b %errorlevel%
