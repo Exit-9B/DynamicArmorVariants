9c60ec4f1fded42a39ab449491568a81aacb669f
Parapets <southwest85b@gmail.com>
2022-10-08 18:40:50 +0100

    Drop VS2019 presets

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-cmake --preset vs2019-windows --check-stamp-file "build/CMakeFiles/generate.stamp" || goto :error
+cmake --preset vs2022-windows --check-stamp-file "build/CMakeFiles/generate.stamp" || goto :error
 cmake --build build --config Release || goto :error
 cmake --install build --component "Fomod" --prefix "package" || goto :error
 cmake --install build --component "Data" --prefix "package/Data" || goto :error
@@ -8,7 +8,7 @@ cmake --install build --component "UIExtensions_Menu" --prefix "package/UIExtens
 cmake --install build --component "HiddenEquipment" --prefix "package/HiddenEquipment" || goto :error
 cmake --install build --component "SKSEPlugin" --prefix "package/SkyrimSE" || goto :error
 
-cmake --preset vs2019-windows-vr --check-stamp-file "buildVR/CMakeFiles/generate.stamp" || goto :error
+cmake --preset vs2022-windows-vr --check-stamp-file "buildVR/CMakeFiles/generate.stamp" || goto :error
 cmake --build buildVR --target "DynamicArmorVariants" --config Release || goto :error
 cmake --install buildVR --component "SKSEPlugin" --prefix "package/SkyrimVR" || goto :error
 
